ebaef7122a721956f6d8c467c9dafa3dfafa88af
susana <susana.casas@iesdoctorbalmis.com>
2020-11-03 12:45:28 +0100

    Agregar archivos de proyecto.

---
+++ Ejercicio6_Las4Estaciones/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace Ejercicio6_Las4Estaciones
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
